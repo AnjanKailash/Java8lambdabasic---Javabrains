bbb8b45507897c513b21db8bf118c52cb2ec6b98
Anjan Kailash <anjan.kailash@gmail.com>
2022-03-17 17:15:01 +0530

    Implementing Runnable interface using Lambda expression

--- src/io/javabrains/RunnableExample.java
+++ src/io/javabrains/RunnableExample.java
@@ -4,14 +4,16 @@ public class RunnableExample {
 
 	public static void main(String[] args) {
 		Thread myThread = new Thread(new Runnable() {
-
 			@Override
 			public void run() {
 				System.out.println("Printed inside runnable");
 			}
-			
 		});
+		myThread.run();
 		
+		//below thing is possible because Runnable is single method interface
+		Thread myLambdaThread = new Thread(() -> System.out.println("Printed inside Lambda runnable"));
+		myLambdaThread.run();
 	}
 
 }
